13836d0ca4a7d6fd16b2c77a1c432611bfaa7280
Noemí Cervero <noemicervero@gmail.com>
2021-05-09 15:20:04 +0200

    BottomsBar :) (falta canviar una a color blanc)

--- components/BottomBar.js
+++ components/BottomBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, StyleSheet, Image } from 'react-native'
+import { View, Text, StyleSheet, Image, Button, Pressable } from 'react-native'
 import Demo from './Demo'
 import Add from './icons/add';
 import Chef from './icons/chef'
@@ -9,19 +9,50 @@ import Settings from './icons/settings';
 export default function BottomBar() {
     return (
         <Demo>
-            <View style={styles.container}>
-                <View style={styles.column}>
-                    {/* <Button props={svg que vols}/> */}
-                    <Settings />
+            {/* BottomBar */}
+            <View style={styles.container1}>
+                <View style={styles.column} onPres>
+                    <Pressable onPress={null}>
+                        <Settings style={styles.icon} />
+                    </Pressable>
                 </View>
                 <View style={styles.column}>
-                    <Chef />
+                    <Pressable onPress={null}>
+                        <Chef style={styles.icon} />
+                    </Pressable>
                 </View>
                 <View style={styles.column}>
-                    <Add />
+                    <Pressable onPress={null}>
+                        <Add style={styles.icon} />
+                    </Pressable>
                 </View>
                 <View style={styles.column}>
-                    <ProfileIcon />
+                    <Pressable onPress={null}>
+                        <ProfileIcon style={styles.icon} />
+                    </Pressable>
+                </View>
+            </View>
+            {/* Variant, provisionalment el fons es gris, sino no es veuen els svg */}
+            <View style={styles.container2}>
+                <View style={styles.column} onPres>
+                    <Pressable onPress={null}>
+                        <Settings style={styles.icon} />
+                    </Pressable>
+                </View>
+                <View style={styles.column}>
+                    <Pressable onPress={null}>
+                        <Chef style={styles.icon} />
+                    </Pressable>
+                </View>
+                <View style={styles.column}>
+                    <Pressable onPress={null}>
+                        <Add style={styles.icon} />
+                    </Pressable>
+                </View>
+                <View style={styles.column}>
+                    <Pressable onPress={null}>
+                        <ProfileIcon style={styles.icon} />
+                    </Pressable>
                 </View>
             </View>
         </Demo>
@@ -29,7 +60,7 @@ export default function BottomBar() {
 }
 
 const styles = StyleSheet.create({
-    container: {
+    container1: {
         flex: 1,
         flexDirection: 'row',
         justifyContent: 'space-between',
@@ -39,10 +70,38 @@ const styles = StyleSheet.create({
         paddingBottom: 10,
         borderTopStartRadius: 20,
         borderTopEndRadius: 20,
-        backgroundColor: '#eee'
+        backgroundColor: '#fff',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.25,
+        elevation: 5,
+    },
+    container2: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        margin: 20,
+        paddingTop: 15,
+        paddingBottom: 10,
+        borderTopStartRadius: 20,
+        borderTopEndRadius: 20,
+        backgroundColor: '#ddd',
     },
     column: {
         flex: 1,
         alignItems: 'center',
+    },
+    icon: {
+        /* shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.25,
+        elevation: 5, */
     }
 })
