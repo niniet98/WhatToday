ab8b01a352323a4a2c535d19f561071a192509cd
JosvilG <vilchezjosep@gmail.com>
2021-05-31 13:55:58 +0200

    Main page + drags + buttons

--- components/Cards.js
+++ components/Cards.js
@@ -1,50 +1,97 @@
-import React from 'react'
-import { View, Text, StyleSheet, Image } from 'react-native'
+import React, { useRef } from "react";
+import { Animated, PanResponder, StyleSheet, View, Image, Button, Alert, Text, IconButton } from "react-native";
 
+const DraggableView = () => {
+    const pan = useRef(new Animated.ValueXY()).current;
 
-
-export default function CardCreation() {
-
-    //const [pressed, setPressed] = useState(false);
-
-    //const extraStyle = pressed ? styles.buttonPressed : styles.buttonNotPressed;
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderMove: Animated.event([
+            null,
+            {
+                dx: pan.x, // x,y are Animated.Value
+                dy: pan.y,
+            },
+        ]),
+        onPanResponderRelease: () => {
+            Animated.spring(
+                pan, // Auto-multiplexed
+                { toValue: { x: 0, y: 0 } } // Back to zero
+            ).start();
+        },
+    });
 
     return (
-
         <View style={styles.container}>
-            <Image style={styles.picture} source={require('../assets/burguer.jpg')} />
-            <Text style={styles.text}>Hamburguesa, 326Kcal</Text>
-        </View>
 
-    )
-}
+            <Animated.View
+                {...panResponder.panHandlers}
+                style={[pan.getLayout(), styles.box]}>
+                <Image style={styles.picture} source={require('../assets/burguer.jpg')} />
+                <Text style={styles.text}>Ola que ase, 580kcal</Text>
+            </Animated.View>
+            <View style={styles.butcontainer}>
+                <View style={styles.button}>
+                    <Button
+                        title="KK"
+                        onPress={() => Alert.alert('Left button pressed')}
+                    />
+                    <Button
+                        title="LIKE"
+                        onPress={() => Alert.alert('Right button pressed')}
+                    />
+                </View>
+            </View>
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: 'row',
-        /* alignItems: 'center', */
-        marginTop: -460,
-        /* marginVertical: 50, */
-        margin: 20,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: -580,
+        marginLeft: 20,
     },
-    picture: {
-        flex: 1,
-        borderRadius: 15,
-        /* alignItems: 'stretch', */
-        resizeMode: "cover",
-        maxHeight: 500,
-    },
-    text: {
-        fontFamily: 'Poppins-Bold',
-        textTransform: 'uppercase',
-        textAlign: 'center',
-        color: "white",
-        fontSize: 20,
+    box: {
         position: 'absolute',
-        bottom: 20,
-        left: 20,
-        shadowColor: 10,
+        backgroundColor: "#61dafb",
+        width: 350,
+        height: 550,
+        borderRadius: 20,
     },
 
+    picture: {
+        width: 350,
+        height: 550,
+        borderRadius: 20,
+        alignItems: "center",
+    },
+    butcontainer: {
+        marginBottom: -555,
+        marginLeft: 0,
+        alignContent: 'center',
+        alignItems: 'center',
+        height: 50,
+        width: 360,
+    },
+    button: {
+        width: 200,
+        padding: 6,
+        position: 'absolute',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+
+    text: {
+        marginTop: 480,
+        marginLeft: 20,
+        fontSize: 24,
+        position: 'absolute',
+        color: 'white',
+    }
 });
+
+export default DraggableView;
