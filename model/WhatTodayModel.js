8db79880c7f09c8279cc6eecbdeaf2db8e070a5f
Noemí Cervero <noemicervero@gmail.com>
2021-06-20 15:52:50 +0200

    Confilcto apiKey arreglado

--- model/WhatTodayModel.js
+++ model/WhatTodayModel.js
@@ -7,7 +7,7 @@ const URL_BASE = 'https://api.spoonacular.com/recipes';
 //const apiKey = "701b25c5a2014c749a3d17ca5e3b357f"; //noe 2
 //15b916dae08f81c59aaff57a425be1c638ad7cbb
 //const apiKey = "19029611d889407a81d175e7ffbebd9f"; //apiKey mario
-const apiKey = "19029611d889407a81d175e7ffbebd9f"; //apiKey mario
+//const apiKey = "19029611d889407a81d175e7ffbebd9f"; //apiKey mario
 //e89966377c73ed879fdda9b8ac57ce1a324259db
 //const apiKey = "f8b7ed4858454125a22606a37be0b9d0"; //apiKey mario 2
 //const apiKey = "40c1982b35bb441a8f82db372ebe7d9c"; //apiKey mario 3
